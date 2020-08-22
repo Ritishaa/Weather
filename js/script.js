24f289ea551153f1c5c226d0d521bebb4aba05f4
Ritishaa <ritisha1029gupta@gmail.com>
2020-08-22 15:28:58 +0530

    Almost ready

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function promptbox(){
  	var loc = window.prompt("Please Enter City Name:");
  	return loc;
@@ -12,12 +13,16 @@ function myapi(){
         }
         console.log(loc);
         //var apkey="3054e14dee01dc11741d8820d2ed3d0d";
-        fetch('http://api.openweathermap.org/data/2.5/weather?q='+loc+'&APPID=3054e14dee01dc11741d8820d2ed3d0d')
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=' + loc + '&APPID=3054e14dee01dc11741d8820d2ed3d0d')
         .then(alldata=>{return alldata.json();})
         .then(_reqdata=>{
-            const {temp, humidity} =_reqdata.main;
+            var {temp, humidity} =_reqdata.main;
             const {name} = _reqdata;
             const {speed} = _reqdata.wind;
+            const {main} = _reqdata.weather[0];
+            var iconcode = _reqdata.weather[0].icon;
+
+            temp=Math.floor(temp-273);
             console.log(temp);   
             console.log(humidity); 
             console.log(name);
@@ -25,10 +30,9 @@ function myapi(){
             document.querySelector(".temperature-degree").innerHTML =temp;
             document.querySelector(".wind").innerHTML = speed;
             document.querySelector(".humidity").innerHTML =humidity;
-
-            //console.log(a);
-           /* a.innerHTML= name;*/
-    });
+            document.querySelector(".description").innerHTML =main;
+            document.querySelector(".ic").src="icon/"+iconcode+".png";
+       });
 }
 
 window.addEventListener('load',()=>{
@@ -36,4 +40,5 @@ window.addEventListener('load',()=>{
    but.addEventListener('click',myapi);
     console.log("hey there");
 })
+
 
